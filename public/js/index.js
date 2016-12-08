b5b906dcd02170e11e6455d0cb650305a1c055d8
rayGreycloud <rayGreycloud@gmail.com>
2016-12-08 09:18:04 -0800

    Add event acknowledgement for new messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,20 @@
 var socket = io();
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  console.log('Connected to server.');
 });
 
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+  console.log('Disconnected from server.');
 });
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+});
+
+socket.emit('createMessage', {
+  from: 'Yoda',
+  text: 'Hello say I.'
+}, function (data) {
+  console.log('Message received.', data);
 });
--- server/server.js
+++ server/server.js
@@ -13,25 +13,26 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+  console.log('New user connected.');
 
   socket.emit('newMessage', generateMessage('Admin',
-  'Welcome to the Chat App'));
+  'Welcome to the Chat App...'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin',
-  'New user joined'));
+  'New user joined.'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from,
     message.text));
+    callback('This is from the server.');
   });
 
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    console.log('User was disconnected.');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}.`);
 });
