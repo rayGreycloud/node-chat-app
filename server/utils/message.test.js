fedee4e11894ec6b3e58d055c783716880087fd8
rayGreycloud <rayGreycloud@gmail.com>
2016-12-09 15:06:59 -0800

    Add generateLocationMessage functionality

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,6 +11,19 @@ describe('generateMessage', () => {
     // expect(res.from).toEqual(from);
     // expect(res.text).toEqual(text);
     expect(res).toInclude({ from, text });
+    expect(res.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Anakin';
+    var latitude = 47.656322;
+    var longitude = -122.315888;
+    var url = 'https://www.google.com/maps?q=47.656322,-122.315888';
+    var res = generateLocationMessage(from, latitude, longitude);
+
+    expect(res).toInclude({ from, url });
     expect(res.createdAt).toBeA('number');
   });
 });
